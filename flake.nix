63887d7fbba8437942d4f641474d6f0eaa791a75
hugosenari <hugosenari@gmail.com>
2022-03-12 04:10:06 -0300

    chore: re index python3

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wrapper to interface with the Python 3 interpreter'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."python3-master".url = "path:./master";
+  inputs."python3-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."python3-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
